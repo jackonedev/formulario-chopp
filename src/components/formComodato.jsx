44c6d55a84dabaed8c370006f1baa67a89647cd5
unknown <jackone.action.software@gmail.com>
2023-07-31 10:03:26 -0300

    reformating form Comodatos Style

--- src/components/formComodato.jsx
+++ src/components/formComodato.jsx
@@ -11,130 +11,136 @@ function FormComodato({ formData, setFormData }) {
 
   return (
     <form className="form-column">
-        <label>
-          Barril 7, 8, 9 Lts:
-          <input
-            type="number"
-            name="barril_7_8_9_litros"
-            className="input-int"
-            value={formData.barril_7_8_9_litros}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Barril 10, 12 Lts:
-          <input
-            type="number"
-            name="barril_10_12_litros"
-            className="input-int"
-            value={formData.barril_10_12_litros}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Barril 18 Lts:
-          <input
-            type="number"
-            name="barril_18_litros"
-            className="input-int"
-            value={formData.barril_18_litros}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Barril 25 Lts:
-          <input
-            type="number"
-            name="barril_25_litros"
-            className="input-int"
-            value={formData.barril_25_litros}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Barril 30 Lts:
-          <input
-            type="number"
-            name="barril_30_litros"
-            className="input-int"
-            value={formData.barril_30_litros}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Barril 40, 50 Lts:
-          <input
-            type="number"
-            name="barril_40_50_litros"
-            className="input-int"
-            value={formData.barril_40_50_litros}
-            onChange={handleChange}
-          />
-        </label>
-
-      <br />
-        <label>
-          Choppera sin barril:
-          <input
-            type="number"
-            name="choppera_sin_barril"
-            className="input-int"
-            value={formData.choppera_sin_barril}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Reductor de presión:
-          <input
-            type="number"
-            name="reductor_presion"
-            className="input-int"
-            value={formData.reductor_presion}
-            onChange={handleChange}
-          />
-        </label>
-        <div>
-          <label>
-            Tubo CO2:
-            <input
-              type="number"
-              name="tubo_CO2"
-              className="input-int"
-              value={formData.tubo_CO2}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            Kg:
-            <input
-              type="number"
-              name="peso_tubo_CO2"
-              className="input-int"
-              value={formData.peso_tubo_CO2}
-              onChange={handleChange}
-            />
-          </label>
+        <div style={{display:"flex"}}>
+          <div style={{display:"grid"}}>
+            <label>
+              Barril 7, 8, 9 Lts:
+              <input
+                type="number"
+                name="barril_7_8_9_litros"
+                className="input-int"
+                value={formData.barril_7_8_9_litros}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Barril 10, 12 Lts:
+              <input
+                type="number"
+                name="barril_10_12_litros"
+                className="input-int"
+                value={formData.barril_10_12_litros}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Barril 18 Lts:
+              <input
+                type="number"
+                name="barril_18_litros"
+                className="input-int"
+                value={formData.barril_18_litros}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Barril 25 Lts:
+              <input
+                type="number"
+                name="barril_25_litros"
+                className="input-int"
+                value={formData.barril_25_litros}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Barril 30 Lts:
+              <input
+                type="number"
+                name="barril_30_litros"
+                className="input-int"
+                value={formData.barril_30_litros}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Barril 40, 50 Lts:
+              <input
+                type="number"
+                name="barril_40_50_litros"
+                className="input-int"
+                value={formData.barril_40_50_litros}
+                onChange={handleChange}
+              />
+            </label>
+          </div>
+          <div style={{display:"grid"}}>
+            <label>
+              Choppera sin barril:
+              <input
+                type="number"
+                name="choppera_sin_barril"
+                className="input-int"
+                value={formData.choppera_sin_barril}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Reductor de presión:
+              <input
+                type="number"
+                name="reductor_presion"
+                className="input-int"
+                value={formData.reductor_presion}
+                onChange={handleChange}
+              />
+            </label>
+            <div style={{ marginLeft: "20px", flexDirection: "column", display: "inline-block" }}>
+              <label>
+                Tubo CO2:
+                <input
+                  type="number"
+                  name="tubo_CO2"
+                  className="input-int"
+                  value={formData.tubo_CO2}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Kg:
+                <input
+                  type="number"
+                  name="peso_tubo_CO2"
+                  className="input-int"
+                  value={formData.peso_tubo_CO2}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+          
+            <label>
+              Válvula automática:
+              <input
+                type="number"
+                name="valvula_automatica"
+                className="input-int"
+                value={formData.valvula_automatica}
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Cabezal 10 Lts:
+              <input
+                type="number"
+                name="cabezal_10_litros"
+                className="input-int"
+                value={formData.cabezal_10_litros}
+                onChange={handleChange}
+              />
+            </label>
+          </div>
         </div>
-        <label>
-          Válvula automática:
-          <input
-            type="number"
-            name="valvula_automatica"
-            className="input-int"
-            value={formData.valvula_automatica}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Cabezal 10 Lts:
-          <input
-            type="number"
-            name="cabezal_10_litros"
-            className="input-int"
-            value={formData.cabezal_10_litros}
-            onChange={handleChange}
-          />
-        </label>
+     
       <br />
         <label>
           Adicionales:
